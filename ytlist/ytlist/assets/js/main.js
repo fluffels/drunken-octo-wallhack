591e7d7ea87af64d34d01ebe8f80c2ab4086994b
Jan CW Kroeze <thejcwk@gmail.com>
2014-08-17 22:09:57 +0200

    Replaced static HTML with AJAX.

--- ytlist/ytlist/assets/js/main.js
+++ ytlist/ytlist/assets/js/main.js
@@ -1,7 +1,25 @@
 /*
  *= require jquery
+ *= require handlebars
  */
 
 $(document).ready(function() {
+    $.ajax("/videos/", {
+        type: "GET",
+        success: function(data) {
+            var div = $("div#content");
+            var template = Handlebars.compile($("#video-template").html());
+            var videos = $.parseJSON(data);
+            for (i in videos)
+            {
+                div.append(template(videos[i]));
+            }
+        },
+        error: function() {
+            var div = $("div#content");
+            div.append("<h1>Failure!</h1");
+            div.append("<p>Could not contact the server.</p>");
+        }
+    });
 });
 
