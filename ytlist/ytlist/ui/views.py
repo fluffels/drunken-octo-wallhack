058aa8322c48eaa0bb9dc21efeda5edcab1b5db4
Jan CW Kroeze <thejcwk@gmail.com>
2014-08-17 20:58:09 +0200

    Made index view display a list of videos.

--- ytlist/ytlist/ui/views.py
+++ ytlist/ytlist/ui/views.py
@@ -1,5 +1,12 @@
 from django.views.generic.base import TemplateView
 
+from ytlist.logic.models import Video
+
 class IndexView(TemplateView):
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['videos'] = Video.objects.all()
+        return context
 
