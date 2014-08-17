8a6cf34eeac4611d15a48787db9cb9621aa2623f
Jan CW Kroeze <thejcwk@gmail.com>
2014-08-17 19:25:05 +0200

    POST logic with tests.

--- ytlist/ytlist/logic/tests.py
+++ ytlist/ytlist/logic/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.http import HttpRequest
+from django.test import TestCase
+from django.test import Client
+
+from .views import VideoAPIView
+
+class VideoAPITestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_post_success(self):
+        response = self.client.post('/videos/', {'data': '{"url": "test"}'})
+        message = json.loads(response.content)
+        self.assertEqual(message['status'], 0)
+
+    def test_post_error(self):
+        response = self.client.post('/videos/')
+        message = json.loads(response.content)
+        self.assertEqual(message['status'], 1)
+
+    def test_parse_error(self):
+        response = self.client.post('/videos/', {'data': '{"test"}'})
+        message = json.loads(response.content)
+        self.assertEqual(message['status'], 2)
+
--- ytlist/ytlist/logic/views.py
+++ ytlist/ytlist/logic/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import HttpResponse
+from django.views.generic import View
+
+class VideoAPIView(View):
+    """Implements the video API described in the README."""
+    http_method_names = ['get', 'post', 'delete']
+
+    def error(self, status, message):
+        """Return an error messsage formatted as a JSON object."""
+
+        error = {"status": status, "message": message}
+        return HttpResponse(json.dumps(error))
+
+    def success(self):
+        """Return a success message formatted as a JSON object."""
+
+        success = {"status": 0, "message": ""}
+        return HttpResponse(json.dumps(success))
+
+    def post(self, request, *args, **kwargs):
+        """Handle POST requests."""
+
+        if ("data" not in request.POST):
+            return self.error(1, ('Please send a valid JSON object in the '
+                                  '"data" field of the POST request.'))
+        else:
+            json_data = request.POST["data"]
+
+            try:
+                video = json.loads(json_data)
+            except Exception as e:
+                return self.error(2, "Error while parsing JSON string '{}': {}".
+                                      format(json_data, e))
+
+            return self.success()
+
