c3b13d14ddc209058b40a568092c37a98ddf8dc0
Jan CW Kroeze <thejcwk@gmail.com>
2014-08-17 20:04:26 +0200

    Forgot URLs file.

--- ytlist/ytlist/urls.py
+++ ytlist/ytlist/urls.py
@@ -3,10 +3,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'ytlist.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from ytlist.logic.views import VideoAPIView
 
+urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^videos/$', VideoAPIView.as_view()),
 )
