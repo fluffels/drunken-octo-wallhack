5fb4217050636844b70d931fa63250ebcb9f53fb
Jan CW Kroeze <thejcwk@gmail.com>
2014-08-17 20:51:39 +0200

    Added an index view.

--- ytlist/ytlist/settings.py
+++ ytlist/ytlist/settings.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ytlist.logic',
+    'ytlist.ui',
 )
 
 MIDDLEWARE_CLASSES = (
--- ytlist/ytlist/urls.py
+++ ytlist/ytlist/urls.py
@@ -5,8 +5,10 @@ admin.autodiscover()
 
 from ytlist.logic.views import VideoAPIView
 from ytlist.logic.views import delete_video
+from ytlist.ui.views import IndexView
 
 urlpatterns = patterns('',
+    url(r'^$', IndexView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^videos/$', VideoAPIView.as_view()),
     url(r'^videos/(?P<id>[0-9]+)/$', delete_video),
