cc080d518f891a6bbdfe54804e4766dcc6c2144f
Jan CW Kroeze <thejcwk@gmail.com>
2014-08-18 21:53:21 +0200

    Factored out submit button code.

--- ytlist/ytlist/assets/js/main.js
+++ ytlist/ytlist/assets/js/main.js
@@ -56,9 +56,8 @@ function setup_csrf()
     });
 }
 
-$(document).ready(function() {
-    setup_csrf();
-
+function load_all_videos()
+{
     $.ajax("/videos/", {
         type: "GET",
         success: function(data) {
@@ -75,37 +74,45 @@ $(document).ready(function() {
             div.append("<p>Could not contact the server.</p>");
         }
     });
+}
 
-    $("#submit-button").click(function() {
-        var url_value = $("#url").val();
-        var re = RegExp("v=(.*?)(&|$)");
-        var match = re.exec(url_value);
+function submit()
+{
+    var url_value = $("#url").val();
+    var re = RegExp("v=(.*?)(&|$)");
+    var match = re.exec(url_value);
 
-        if (match)
-        {
-            url_value = match[1];
-        }
+    if (match)
+    {
+        url_value = match[1];
+    }
 
-        var obj = {url: url_value};
-        var post_data = "data=" + JSON.stringify(obj);
-        $.ajax("/videos/", {
-            type: 'POST',
-            data: post_data,
-            success: function(msg) {
-                var message = $.parseJSON(msg);
-                if (message.status === 0)
-                {
-                    add_video_by_id(message.message);
-                }
-                else
-                {
-                    $("div#add-messages").append("<p>Could not post video: " + message.message + "</p>");
-                }
-            },
-            error: function(msg) {
-                $("div#add-messages").append("<p>Could not contact the server.</p>");
+    var obj = {url: url_value};
+    var post_data = "data=" + JSON.stringify(obj);
+
+    $.ajax("/videos/", {
+        type: 'POST',
+        data: post_data,
+        success: function(msg) {
+            var message = $.parseJSON(msg);
+            if (message.status === 0)
+            {
+                add_video_by_id(message.message);
             }
-        });
+            else
+            {
+                $("div#add-messages").append("<p>Could not post video: " + message.message + "</p>");
+            }
+        },
+        error: function(msg) {
+            $("div#add-messages").append("<p>Could not contact the server.</p>");
+        }
     });
+}
+
+$(document).ready(function() {
+    setup_csrf();
+    load_all_videos();
+    $("#submit-button").click(submit);
 });
 
